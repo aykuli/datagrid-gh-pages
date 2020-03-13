6dc0830d8fc142d2bbded4f07c20561f5153e940
aykuli <aykuli@ya.ru>
2020-03-13 09:49:43 +0700

    feat: datagrid simple

---
+++ docs/precache-manifest.6c1063933eaaaf2a3b8cf306171eccd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b174707cc6d3d1dbac278f8c42739f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75867bef5d284d7fe73c",
+    "url": "./static/css/2.df06f0d4.chunk.css"
+  },
+  {
+    "revision": "75867bef5d284d7fe73c",
+    "url": "./static/js/2.1070f96f.chunk.js"
+  },
+  {
+    "revision": "7be0eacaf502f4a26dea1bcf5e16461d",
+    "url": "./static/js/2.1070f96f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39e788bde7022dc1f34",
+    "url": "./static/js/main.be9d5d55.chunk.js"
+  },
+  {
+    "revision": "e6398e6ef302edff7b00",
+    "url": "./static/js/runtime-main.23a781e4.js"
+  },
+  {
+    "revision": "7211ed5481e912a5cb76155b123ff665",
+    "url": "./static/media/ubuntu-condensed-cyrillic-400.7211ed54.woff2"
+  },
+  {
+    "revision": "e7b18070cf7ab8b5e7189aae98afcd3d",
+    "url": "./static/media/ubuntu-condensed-cyrillic-400.e7b18070.woff"
+  }
+]);
